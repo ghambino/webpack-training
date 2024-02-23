d234734d77e530bc9bac6d45cfd532537b7fd207
Abdulwahab Abbas <abbas@seamlesshr.com>
2024-02-23 11:23:09 +0100

    learning how to pass environmental variable from the webpack cli to the config file....also the webpack config file must be changes to a function that returns an config object

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports =  {
-    entry: {
-       index: './src/index.js',
-    //    print: './src/print.js',
-    },
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: "./dist",
-    },
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Caching'
-        })
-    ],
-    optimization: {
-        moduleIds: 'deterministic',
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
+module.exports = (env) => {
+    console.log('welcome to my production environment:', env.production);
+    return {
+        entry: {
+           index: './src/index.js',
+        //    print: './src/print.js',
+        },
+        mode: 'development',
+        devtool: 'inline-source-map',
+        devServer: {
+            static: "./dist",
+        },
+        output: {
+            filename: '[name].[contenthash].js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Caching'
+            })
+        ],
+        optimization: {
+            moduleIds: 'deterministic',
+            runtimeChunk: 'single',
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all',
+                    }
                 }
             }
         }
